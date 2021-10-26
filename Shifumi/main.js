394fa18248914a7209651fbdb0a2b15861b19171
ChuckyMaster <67459259+ChuckyMaster@users.noreply.github.com>
2021-10-26 13:03:09 +0200

    avancement shifumi

--- Shifumi/main.js
+++ Shifumi/main.js
@@ -1,11 +1,18 @@
 "use stricct";
-
+//variables
 const btn = document.querySelector(".btn");
 
 let urChoice;
 let computer;
 let pfc = ["pierre", "feuille", "ciseaux"];
+let resultat;
 
+//fonctions
+function computerChoice() {
+  return Math.floor(Math.random() * 3);
+}
+
+//code principal
 do {
   urChoice = prompt("veuillez choisir : pierre, feuille ou ciseaux");
 } while (
@@ -14,13 +21,27 @@ do {
   urChoice !== "ciseaux"
 );
 
-function computerChoice() {
-  return Math.floor(Math.random() * 3);
+const randompfc = computerChoice();
+computer = pfc[randompfc];
+console.log(computer);
+
+if (urChoice === computer) {
+  resultat = "égalité";
+} else if (
+  (urChoice === "ciseaux" && computer === "pierre") ||
+  (urChoice === "pierre" && computer === "feuille")
+) {
+  resultat = "vous avez perdu";
+} else
+  (urChoice === "ciseaux" && computer === "feuille") ||
+    (urChoice === "feuille" && computer === "pierre") ||
+    (urChoice === "pierre" && computer === "ciseaux");
+{
+  resultat = "Vous gagnez!!";
 }
 
-btn.addEventListener("click", function computerPlay() {
-  const randompfc = computerChoice();
-  console.log(randompfc);
-  computer = pfc[randompfc];
-});
-console.log(computer);
+document.querySelector(
+  "#content"
+).innerHTML += ` vous avez choisi  <img src="./img/${urChoice}.png" alt="ta choisi ca"> <br>  le pc à choisi  <img src="./img/${computer}.png" alt="ta choisi ca" <br> <stron> <p> ${resultat} </p> </strong> `;
+
+console.log(resultat);
