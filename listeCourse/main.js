9c4b19a63b421e348621e242779e8acd30a64dd2
ChuckyMaster <67459259+ChuckyMaster@users.noreply.github.com>
2021-10-26 18:54:22 +0200

    debut liste

---
+++ listeCourse/main.js
@@ -0,0 +1,38 @@
+"use strict";
+
+// VARIABLES
+
+let list = new Array();
+
+// FONCTIONS
+
+/****
+ * ajouter un item à la liste de course
+ * @param {string} item
+ */
+
+function addItem(item) {
+  list.push(item);
+}
+
+list.forEach(function (item, index) {
+  console.log(item, index);
+});
+
+console.log(list);
+
+/***
+ *
+ * @returns le contenu de la liste et son nombre d'items
+ *
+ */
+function display() {
+  return ` Il ya ${list.length} produits dans la liste.`;
+}
+
+addItem("patate");
+addItem("ravioli");
+addItem("concombre amer");
+addItem("choux fleur");
+
+document.querySelector("#content").innerHTML = display();
