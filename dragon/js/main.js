d147af2966aa088ec2de872f75a72672c0136001
ChuckyMaster <67459259+ChuckyMaster@users.noreply.github.com>
2021-10-28 16:22:23 +0200

    fin du projet

--- dragon/js/main.js
+++ dragon/js/main.js
@@ -3,8 +3,6 @@
 //VARIABLES
 
 let game = new Object();
-// game.hpDragon;
-// game.hpChevalier;
 
 game.armes;
 game.armure;
@@ -12,37 +10,191 @@ game.difficulty;
 game.chevDammage;
 game.dragDammage;
 
-// FONCTION
+//FONCTIONS
 
+function initializeGame() {
+  switch (game.difficulty) {
+    case 1:
+      "Le dragon a", (game.hpDragon = getRandom(150, 250)), " Points de vie";
+
+      "vous avez", (game.hpChevalier = getRandom(200, 250));
+      "point de vie";
+
+      break;
+
+    case 2:
+      "Le dragon a", (game.hpDragon = getRandom(200, 250));
+      " Points de vie";
+
+      "vous avez", (game.hpChevalier = getRandom(200, 250)), "point de vie";
+
+      break;
+
+    case 3:
+      "Le dragon a", (game.hpDragon = getRandom(200, 250));
+      " Points de vie";
+
+      "vous avez", (game.hpChevalier = getRandom(150, 200));
+      "point de vie";
+      break;
+  }
+
+  switch (game.armes) {
+    case 1:
+      game.armes = 0.5;
+      break;
+    case 2:
+      game.armes = 0;
+
+    case 3:
+      game.armes = 0.5;
+      break;
+  }
+
+  switch (game.armure) {
+    case 2:
+      game.armure = 0.25;
+      break;
+    case 2:
+      game.armure = 0;
+    case 3:
+      game.armure = 0.5;
+      break;
+  }
+
+  console.log(
+    " vous avez choisi la diffuculté numéro",
+    game.difficulty,
+    " l'armure du dragon a",
+    game.armure,
+    " de debuff",
+    " Vous avez choisi l'arme numero ( oui numéro)",
+    game.armes
+  );
+  return game;
+}
+
+function gameLoop() {
+  let speed;
+  let degats;
+  let dragonAtack;
+  let chevAttack;
+  let speedDrag;
+  let speedChev;
+  let i = 1;
+  do {
+    console.log("round ", i);
+
+    speedDrag = getRandom(10, 20);
+    console.log("le dragon a tiré au sort le numero: ", speedDrag);
+    speedChev = getRandom(10, 20);
+    console.log("le chevalier a tiré au sort le numero: ", speedChev);
+    if (speedChev == speedDrag) continue;
+    if (speedChev > speedDrag) {
+      chevAttack = attackFromChevalier();
+      game.hpDragon -= chevAttack;
+    } else {
+      dragonAtack = attackFromDragon();
+      game.hpChevalier -= dragonAtack;
+
+      document.querySelector("#game").innerHTML += `  <h3>Personnage PV</h3>
+        <table>
+         
+        </table> <h3>---- Tour numero: ${i}----</h3> <p>Le dragon est plus rapide que vous et vous met ${chevAttack} de degats</p> `;
+    }
+    document.querySelector("#game").innerHTML += `  <h3>Personnage PV</h3>
+    <table>
+    <tr>
+      <td>chevalier</td>
+      <td>${game.hpChevalier}</td>
+      </tr>
+    <tr>
+      <td>Dragon</td>
+      <td>${game.hpDragon}</td>
+      </tr>
+    </table> <h3>---- Tour numero: ${i}----</h3> <p>Vous êtes plus rapide que le dragon et vous lui infligés ${chevAttack} de degats</p> `;
+
+    i++;
+  } while (
+    // speed = getRandom(10, 20);
+
+    game.hpChevalier > 0 &&
+    game.hpDragon > 0
+  );
+  return;
+}
+
+function gameStart() {
+  game.difficulty = requestInteger(
+    "Choisissez le difficulté: n/ 1-facile 2-moyen 3-difficile",
+    1,
+    3
+  );
+
+  game.armure = requestInteger(
+    "Choisissez votre armure: \n 1-cuivre 2-fer 3-magique ",
+    1,
+    3
+  );
+
+  game.armes = requestInteger(
+    "Choisissez votre arme: \n 1-bois 2-acier 3-Excalibur",
+    1,
+    3
+  );
+  initializeGame(game.difficulty, game.armure, game.armes);
+
+  gameLoop();
+  showGameWinner();
+}
+
+function showGameWinner() {
+  let inner = document.querySelector("#game");
+
+  if (game.hpChevalier <= 0) {
+    inner.insertAdjacentHTML(
+      "beforebegin",
+      ` <article><p>le gagnant est: LE DRAGON  </p> <img  src="./img/dragon.png" alt=""> </p>`
+    );
+  } else {
+    inner.insertAdjacentHTML(
+      "beforebegin",
+      ` <article> <p>le gagnant est: LE DRAGON </p> <img  src="./img/knight.png" alt=""> `
+    );
+  }
+
+  return;
+}
+function attackFromDragon() {
+  let dammage;
+  if (game.difficulty == 1) {
+    dammage = getRandom(10, 20) * game.armure;
+  } else if (game.difficulty == 2) {
+    dammage = getRandom(20, 30) * game.armure;
+  } else {
+    dammage = getRandom(20, 30) * game.armure;
+  }
+  console.log("here from dragon", dammage);
+  return dammage;
+}
+
+function attackFromChevalier() {
+  let dammage;
+  if (game.difficulty == 1) {
+    dammage = getRandom(25, 30) * game.armes;
+  } else if (game.difficulty == 2) {
+    dammage = getRandom(15, 20) * game.armes;
+  } else {
+    dammage = getRandom(5, 10) * game.armes;
+  }
+  console.log("here from chev", dammage);
+  return dammage;
+}
 //PRINCIPAL
 
 // appel aux fonctions
-game.difficulty = requestInteger(
-  "Choisissez le difficulté: n/ 1-facile 2-moyen 3-difficile",
-  1,
-  3
-);
 
-game.armure = requestInteger(
-  "Choisissez votre armure: \n 1-cuivre 2-fer 3-magique ",
-  1,
-  3
-);
-
-game.armes = requestInteger(
-  "Choisissez votre arme: \n 1-bois 2-acier 3-Excalibur",
-  1,
-  3
-);
-
-game = initializeGame(game.difficulty, game.armure, game.armes);
-gameLoop();
-attackFromChevalier();
-attackFromDragon();
-
-// console.log("vous avez choisi la difficulté", game.difficulty);
-// console.log("vous avez ce buff ", game.armure, "en armure");
-// console.log("vous avez choisi ", game.armes, "comme arme");
+gameStart();
 
 console.log(
   "le chevalier a",
@@ -51,14 +203,3 @@ console.log(
   game.hpDragon,
   " points de vie"
 );
-
-// attackFromDragon(game.difficulty, game.dragDammage, game.armure);
-// attackFromChevalier(game.difficulty, game.chevDammage, game.armes);
-
-// do {} while (game.hpChevalier > 0 && game.hpDragon > 0);
-
-// TEST DE LA FONCTION REQUESTION
-// console.log(
-//   "question difficulté",
-//   requestInteger("tu veux cb entre 5 balle et 8 balles", 5, 8)
-// );
--- dragon/js/utilities.js
+++ dragon/js/utilities.js
@@ -8,34 +8,8 @@
 //FONCTIONS
 
 function getRandom(min, max) {
-  let number = min + Math.floor(Math.random()) * (max - min);
+  let number = Math.floor(Math.random() * (max - min + 1) + min);
   return number;
-}
-
-function attackFromDragon() {
-  let dammage;
-  if (game.difficulty == 1) {
-    dammage = getRandom(10, 20) * game.armure;
-  } else if (game.difficulty == 2) {
-    dammage = getRandom(20, 30) * game.armure;
-  } else {
-    dammage = getRandom(20, 30) * game.armure;
-  }
-  console.log("here from dragon", dammage);
-  return dammage;
-}
-
-function attackFromChevalier() {
-  let dammage;
-  if (game.difficulty == 1) {
-    dammage = getRandom(25, 30) * game.armes;
-  } else if (game.difficulty == 2) {
-    dammage = getRandom(15, 20) * game.armes;
-  } else {
-    dammage = getRandom(5, 10) * game.armes;
-  }
-  console.log("here from chev", dammage);
-  return dammage;
 }
 
 function requestInteger(question, min, max) {
@@ -46,111 +20,22 @@ function requestInteger(question, min, max) {
   return number;
 }
 
+// function display() {
+//   let htmlTab = "<table>";
+//   "le chevalier attaque et inflige :", chevAttack, " de degats";
+//   htmlTab += "</table>";
+//   return;
+// }
 // APPEL TOUTES LES FONCTION POUR ENREGISTRER LES VALEURS
-function initializeGame() {
-  switch (game.difficulty) {
-    case 1:
-      "Le dragon a", (game.hpDragon = getRandom(150, 250)), " Points de vie";
 
-      "vous avez", (game.hpChevalier = getRandom(200, 250));
-      "point de vie";
+// document.querySelector("#game").innerHTML += `  h3>Personnage PV</h3>
+// <table>
+//   <td>Chevalier</td>
+//   <td>${game.hpDragon}</td>
+// </table> <h3>---- Tour numero: ${i}----</h3> <p>Le dragon est plus rapide que vous et vous met ${chevAttack} de degats</p> `;
 
-      break;
-
-    case 2:
-      "Le dragon a", (game.hpDragon = getRandom(200, 250));
-      " Points de vie";
-
-      "vous avez", (game.hpChevalier = getRandom(200, 250)), "point de vie";
-
-      break;
-
-    case 3:
-      "Le dragon a", (game.hpDragon = getRandom(200, 250));
-      " Points de vie";
-
-      "vous avez", (game.hpChevalier = getRandom(150, 200));
-      "point de vie";
-      break;
-  }
-
-  switch (game.armes) {
-    case 1:
-      game.armes = 0.5;
-      break;
-    case 2:
-      game.armes = 0;
-
-    case 3:
-      game.armes = 0.5;
-      break;
-  }
-
-  switch (game.armure) {
-    case 2:
-      game.armure = 0.25;
-      break;
-    case 2:
-      game.armure = 0;
-    case 3:
-      game.armure = 0.5;
-      break;
-  }
-
-  console.log(
-    " vous avez choisi la diffuculté numéro",
-    game.difficulty,
-    " l'armure du dragon a",
-    game.armure,
-    " de debuff",
-    " Vous avez choisi l'arme numero ( oui numéro)",
-    game.armes
-  );
-  return game;
-}
-
-function gameLoop() {
-  let speed;
-  let degats;
-  let dragonAtack;
-  let chevAttack;
-  let speedDrag;
-  let speedChev;
-  let i = 1;
-  do {
-    console.log("round ", i);
-
-    do {
-      speedDrag = getRandom(10, 20);
-      console.log("le dragon a tiré au sort le numero: ", speedDrag);
-      speedChev = getRandom(10, 20);
-      console.log("le chevalier a tiré au sort le numero: ", speedChev);
-
-      if (speedChev > speedDrag) {
-        chevAttack = attackFromChevalier();
-        game.hpDragon -= chevAttack;
-        console.log(
-          "le chevalier attaque et inflige :",
-          chevAttack,
-          " de degats"
-        );
-      } else {
-        dragonAtack = attackFromDragon();
-        game.hpChevalier -= dragonAtack;
-        console.log(
-          "le chevalier attaque et inflige :",
-          dragonAtack,
-          " de degats"
-        );
-      }
-    } while (speedDrag !== speedChev);
-
-    i++;
-  } while (
-    // speed = getRandom(10, 20);
-
-    game.hpChevalier > 0 &&
-    game.hpDragon > 0
-  );
-  console.log(game.hpChevalier, degats);
-}
+// document.querySelector("#game").innerHTML += `  h3>Personnage PV</h3>
+//       <table>
+//         <td>Chevalier</td>
+//         <td>${game.hpChevalier}</td>
+//       </table> <h3>---- Tour numero: ${i}----</h3> <p>Vous êtes plus rapide que le dragon et vous lui infligés ${chevAttack} de degats</p> `;
